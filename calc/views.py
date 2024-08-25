79579ccd17a93d42e5cf4d4476da7fed411e0e1f
alfredolandi97 <alfredo.landi@mail.polimi.it>
2024-08-26 00:01:26 +0200

    Bugs fixed

--- calc/views.py
+++ calc/views.py
@@ -17,28 +17,28 @@ def home(request):
         series_id = random.randint(1, 48964)
         response = requests.get('https://www.episodate.com/api/show-details?q=' + str(series_id)).json()
         series = {
-            "id": response.tvShow.id,
-            "name": response.tvShow.name,
-            "permalink": response.tvShow.permalink,
-            "url": response.tvShow.url,
-            "description": response.tvShow.description,
-            "descriptionSource": response.tvShow.description_source,
-            "start_date": response.tvShow.start_date,
-            "end_date": response.tvShow.end_date,
-            "country": response.tvShow.country,
-            "status": response.tvShow.status,
-            "runtime": response.tvShow.runtime,
-            "network": response.tvShow.network,
-            "youtube_link": response.tvShow.youtube_link,
-            "image_path": response.tvShow.image_path,
-            "image_thumbnail_path": response.tvShow.image_thumbnail_path,
-            "rating": response.tvShow.rating,
-            "rating_count": response.tvShow.rating_count,
-            "countdown": response.tvShow.countdown,
-            "genres": response.tvShow.genres,
-            "pictures": response.tvShow.pictures,
-            "episodes": response.tvShow.episodes
+            "id": response["tvShow"]["id"],
+            "name": response["tvShow"]["name"],
+            "permalink": response["tvShow"]["permalink"],
+            "url": response["tvShow"]["url"],
+            "description": response["tvShow"]["description"],
+            "descriptionSource": response["tvShow"]["description_source"],
+            "start_date": response["tvShow"]["start_date"],
+            "end_date": response["tvShow"]["end_date"],
+            "country": response["tvShow"]["country"],
+            "status": response["tvShow"]["status"],
+            "runtime": response["tvShow"]["runtime"],
+            "network": response["tvShow"]["network"],
+            "youtube_link": response["tvShow"]["youtube_link"],
+            "image_path": response["tvShow"]["image_path"],
+            "image_thumbnail_path": response["tvShow"]["image_thumbnail_path"],
+            "rating": response["tvShow"]["rating"],
+            "rating_count": response["tvShow"]["rating_count"],
+            "countdown": response["tvShow"]["countdown"],
+            "genres": response["tvShow"]["genres"],
+            "pictures": response["tvShow"]["pictures"],
+            "episodes": response["tvShow"]["episodes"]
             }
         rec_list.append(series)
     
-    return JsonResponse(rec_list)
+    return JsonResponse(rec_list, safe=False)
