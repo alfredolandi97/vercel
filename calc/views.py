7248869c10329441912ac0eae1506ed765824f7f
alfredolandi97 <alfredo.landi@mail.polimi.it>
2024-08-25 19:53:45 +0200

    new version of the recommender server

--- calc/views.py
+++ calc/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import random
+import requests
 
 # Create your views here.
 def home(request):
@@ -13,6 +14,31 @@ def home(request):
     rec_list = []
     
     for i in range(10):
-        rec_list.append(random.randint(1, 1001))
+        series_id = random.randint(1, 48964)
+        response = requests.get('https://www.episodate.com/api/show-details?q=' + str(series_id)).json()
+        series = {
+            "id": response.tvShow.id,
+            "name": response.tvShow.name,
+            "permalink": response.tvShow.permalink,
+            "url": response.tvShow.url,
+            "description": response.tvShow.description,
+            "descriptionSource": response.tvShow.description_source,
+            "start_date": response.tvShow.start_date,
+            "end_date": response.tvShow.end_date,
+            "country": response.tvShow.country,
+            "status": response.tvShow.status,
+            "runtime": response.tvShow.runtime,
+            "network": response.tvShow.network,
+            "youtube_link": response.tvShow.youtube_link,
+            "image_path": response.tvShow.image_path,
+            "image_thumbnail_path": response.tvShow.image_thumbnail_path,
+            "rating": response.tvShow.rating,
+            "rating_count": response.tvShow.rating_count,
+            "countdown": response.tvShow.countdown,
+            "genres": response.tvShow.genres,
+            "pictures": response.tvShow.pictures,
+            "episodes": response.tvShow.episodes
+            }
+        rec_list.append(series)
     
-    return JsonResponse({id:rec_list})
+    return JsonResponse(rec_list)
