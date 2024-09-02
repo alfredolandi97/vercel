bea3414f89eca89a667c1d7e24c00316696211f1
alfredolandi97 <alfredo.landi@mail.polimi.it>
2024-09-02 15:00:50 +0200

    recommender sources added

--- calc/views.py
+++ calc/views.py
@@ -2,19 +2,37 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import random
 import requests
+import pandas as pd
 
 # Create your views here.
 def home(request):
     id = int(request.GET["id"])
-    #id=1
-    """
-    Recommender calcola le raccomandazioni sulla base delle preferenze dell'id x
-    Filla la lista da passare in JSON
-    """
+
+    data_path = "cals/resources/"
+    data_file_name = data_path + "output.csv"
+
+    
+    recommendations = pd.read_csv(
+        filepath_or_buffer=data_file_name,
+        sep=","
+    )
+
     rec_list = []
+
+    recommendationsList = []
+    isUserKnown = False
+    if(id in recommendations["user_id"]):
+        isUserKnown = True
+        userRecommendations = recommendations[recommendations["user_id"]==id]
+        recommendationsList = list(userRecommendations["item_list"])
+        recommendationsList = recommendationsList[0].split()
     
     for i in range(10):
-        series_id = random.randint(1, 48964)
+        if(isUserKnown):
+            series_id = int(recommendationsList[i])
+        else:
+            series_id = random.randint(1, 48964)
+            
         response = requests.get('https://www.episodate.com/api/show-details?q=' + str(series_id)).json()
         series = {
             "id": response["tvShow"]["id"],
